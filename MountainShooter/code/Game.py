25825fc65fd1dc074ca756fbd758e66c72e492be
rfrancodev <francorfs@icloud.com>
2025-02-22 19:54:47 -0300

    Creating asset folder Inserting all assets Creating the menu: backgrund, sound and fonts Creating the const file

--- MountainShooter/code/Game.py
+++ MountainShooter/code/Game.py
@@ -1,23 +1,16 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WINDOW_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WINDOW_HEIGHT))
 
     def run(self):
-        pass
         while True:
             menu = Menu(self.window)
-                menu.run()
-                pass
-
-            # Check All events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # Close window
-            #         quit()  # End pygame
-
+            menu.run()
+            pass
--- MountainShooter/code/Menu.py
+++ MountainShooter/code/Menu.py
@@ -11,12 +11,12 @@ from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
+        self.surf = pygame.image.load('../asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
         menu_option = 0
-        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.load('../asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
             # DRAW IMAGES
